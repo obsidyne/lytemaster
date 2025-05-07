fa84c87b30e1c00d7a01033915512f7a07ff1314
obsidyne <obsidyne@gmail.com>
2025-05-07 19:53:14 +0530

    ewdwe

--- src/assets/placeholders.js
+++ src/assets/placeholders.js
@@ -4,9 +4,9 @@ import gmbhPlaceholder from "@assets/landing/gmbh-placeholder.jpeg";
 import { bento } from "@components/projects/bentoGrid";
 
 /* Product thumbnails */
-import industrialLightingThumbnail from "@assets/landing/industrial-lighting-thumbnail2.jpeg";
+import industrialLightingThumbnail2 from "@assets/landing/industrial-lighting-thumbnail2.jpeg";
 import commercialLightingThumbnail from "@assets/landing/commercial-lighting-thumbnail.jpeg";
-import landscapeLightingThumbnail from "@assets/landing/landscape-lighting-thumbnail2.jpeg";
+import landscapeLightingThumbnail2 from "@assets/landing/landscape-lighting-thumbnail2.jpeg";
 import contactUsImage from "@assets/landing/contact-us.jpg";
 
 /* Industries placeholder images */
